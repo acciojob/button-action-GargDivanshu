90bd1ddb831843743ef7cac53f20c40b7a1c5964
GargDivanshu <divanshu.garg113@gmail.com>
2024-02-07 22:28:04 +0530

    sol checking

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 import './../styles/App.css';
 
-const App = (props) => {
+export default function App(props) {
+  const [flag, setFlag] = useState(false);
 
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      <p id="para" className={flag ? "show" : "hide"}>
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
+      <button id="click" onClick={() => setFlag(!flag)}>
+        Click
+      </button>
     </div>
   );
 }
-
-export default App
